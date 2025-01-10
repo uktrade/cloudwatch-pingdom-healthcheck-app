56c53b801507dd882307ca3e7e47d0cfd14b1410
Saipriya Madapati <saipriyamadapati@DBT000494.local>
2025-01-10 15:11:05 +0000

    codebase prep for migration to DBT platform

--- .copilot/phases/install.sh
+++ .copilot/phases/install.sh
@@ -3,4 +3,9 @@
 # Exit early if something goes wrong
 set -e
 
+apt-get update && apt-get install -y libxml2
+
+pip install requirements.txt
+
+
 # Add commands below to run as part of the install phase
