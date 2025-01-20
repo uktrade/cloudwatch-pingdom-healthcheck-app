134188223c49c71a4749cba6003f21e1a8f4ed98
Saipriya Madapati <saipriyamadapati@DBT000494.local>
2025-01-20 12:18:27 +0000

    copilot files updated

--- .copilot/phases/install.sh
+++ .copilot/phases/install.sh
@@ -3,6 +3,4 @@
 # Exit early if something goes wrong
 set -e
 
-pip install -r requirements.txt
-
 # Add commands below to run as part of the install phase
--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -3,6 +3,4 @@
 # Exit early if something goes wrong
 set -e
 
-pip3 install -r requirements.txt
-
 # Add commands below to run as part of the pre_build phase
